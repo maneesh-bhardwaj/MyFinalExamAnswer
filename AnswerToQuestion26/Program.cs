b1e2cc54e6c4b9595c76dd9810da3592e347a987
Maneesh <bhardwaj.maneesh@outlook.com>
2020-07-02 20:08:38 -0400

    John is the best teacher!

---
+++ AnswerToQuestion26/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace AnswerToQuestion26
+{
+    public abstract class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    class Adult: Person
+    {
+        public Adult(string fName, string lName)
+        {
+            this.FirstName = fName;
+            this.LastName = lName;
+        }
+    }
+    class Child: Person
+    {
+        public Child(string fName, string lName)
+        {
+            this.FirstName = fName;
+            this.LastName = lName;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] people = new Person[10];
+            people[0] = new Adult("Maneesh","Bhardwaj");
+            people[1] = new Child("Krishiv","Bhardwaj");
+
+            foreach (var item in people.Where(x=> x != null))
+            {
+                Console.WriteLine($"{item.FirstName}  {item.LastName}");
+            }
+        }
+    }
+}
